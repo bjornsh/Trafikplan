4210cd3aa51e3e1a7b41891c12ac6849fec45c39
bjornsh <bjorn.schulte-herbruggen@ul.se>
2020-06-09 11:12:04 +0200

    improved documentation

--- 00_skript/aktiviteter.R
+++ 00_skript/aktiviteter.R
@@ -1,8 +1,25 @@
+#---------------------------------------------------------------------------------------------------
+# Syfte
+#---------------------------------------------------------------------------------------------------
 
-# indikator 4.2
-# Från alla hpl i alla medelstora tätorter (1000-XXX befolkning) som är inte kommunhuvudort till alla respektive 
-# kommunhuvudorter i länet finns minst 1 resmöjlighet med max 1 byte mellan 1900 och 2159 på vardagar och helger, och i motsatt riktning.
+## Utvärdera TFP indikator 4.2
+## Indikator tolkning: Från alla hpl i alla medelstora tätorter (1000-6999 befolkning) som är inte kommunhuvudort till alla respektive 
+## kommunhuvudorter i länet finns minst 1 resmöjlighet med max 1 byte mellan 1900 och 2159 på vardagar och helger
+## plus en resemöjlighet i motsatt riktning inom samma tidsperiod (för att komma hem).
 
+
+
+
+#---------------------------------------------------------------------------------------------------
+# Städa
+#---------------------------------------------------------------------------------------------------
+rm(list = ls())
+invisible(gc())
+
+
+#---------------------------------------------------------------------------------------------------
+# Libraries
+#---------------------------------------------------------------------------------------------------
 library(tidyr)
 library(dplyr)
 library(rgdal)
@@ -10,13 +27,15 @@ library(rgeos)
 library(raster)
 library(jsonlite)
 
-rm(list = ls())
-invisible(gc())
-
 
 `%notin%` <- Negate(`%in%`)
 
-# ladda ner, unzip och importera tätort shp till R
+
+#---------------------------------------------------------------------------------------------------
+# Hämta data
+#---------------------------------------------------------------------------------------------------
+
+## tätort shp
 td = tempdir()
 tf = tempfile(tmpdir=td, fileext=".zip")
 
@@ -30,15 +49,19 @@ tatort <- readOGR(paste0(td2, "/To2018_Swe99TM.shp"), stringsAsFactors = FALSE, 
 unlink(td)
 
 
-# tre SCB tätorter är del av ULs stadstrafik och kategoriseras om till Uppsala tätort
-# Ultuna (T0654), Sävja (T0632), Håga (T0566)
+#---------------------------------------------------------------------------------------------------
+# Datahantering
+#---------------------------------------------------------------------------------------------------
+
+## tre SCB tätorter är del av ULs stadstrafik och kategoriseras om till Uppsala tätort
+## Ultuna (T0654), Sävja (T0632), Håga (T0566)
 omkateg = c("T0654", "T0632", "T0566") 
 tatort@data = tatort@data %>% 
   mutate(TATORT = ifelse(TATORTSKOD %in% omkateg, "Uppsala", TATORT))
 
 
-# identifiera medelstora tätorter som är inte kommun huvudtätort
-# Bålsta = huvudort för Håbo kommun; Skutskär = huvudort för Älvkarleby kommun
+## identifiera medelstora tätorter som är inte kommun huvudtätort
+## Bålsta = huvudort för Håbo kommun; Skutskär = huvudort för Älvkarleby kommun. För andra kommuner är kommunnamn = huvudortnamn
 huvudort = c("Enköping", "Heby", "Bålsta", "Knivsta", "Tierp", "Uppsala", "Skutskär", "Östhammar")
 
 medelstor.shp = tatort[tatort@data$LAN_NAMN == "Uppsala län" &
@@ -49,33 +72,33 @@ medelstor.shp = tatort[tatort@data$LAN_NAMN == "Uppsala län" &
 huvudort.shp = tatort[tatort@data$TATORT %in% huvudort,]
 
 
-# hämta tätort ID för medelstora- och huvudtätorter 
+## hämta tätort ID för medelstora- och huvudtätorter 
 medelstortatortkod = medelstor.shp@data$TATORTSKOD
 medelstortatortnamn = medelstor.shp@data$TATORT
 
 huvudortkod = huvudort.shp@data$TATORTSKOD
 
-####### identifiera relevanta hpl per tätorttyp
+## identifiera relevanta hpl per tätorttyp
 HplIdKoordinat = read.csv2("01_input_data/HplData_2020-05-19.csv")
 
 HplMedelStoraTat = HplIdKoordinat %>% filter(TatortKod %in% medelstortatortkod)
 HplHuvudTat = HplIdKoordinat %>% filter(TatortKod %in% huvudortkod)
 
 
-# skapa en df med hpl ID och tätort namn för medel och huvud tätorter 
+## skapa en df med hpl ID och tätort namn för medel och huvud tätorter 
 AllaHplMedelHuvud = HplMedelStoraTat[,c("HplID", "TatortNamn")] %>% 
   bind_rows(., HplHuvudTat[,c("HplID", "TatortNamn")])
 
 
-# länk medelstora tätorter till respektive huvudtätort
-# ladda df med en hpl per tätort
+## länk medelstora tätorter till respektive huvudtätort
+## ladda df med en hpl per tätort
 hpl = read.csv("01_input_data/TatortMedEnHpl.csv", sep = ";", 
                stringsAsFactors=FALSE, encoding = "UTF-8")
 colnames(hpl) = c("TatortNamn", "HplNamn", "HplID")
 
 
-# eftersom det måste finnas resmöjligheter till huvudtätorten och hem
-# skapas här en df med start och stop hpl i båda riktningar
+## eftersom det måste finnas resmöjligheter till huvudtätorten och hem
+## skapas en df med start och stop hpl i båda riktningar
 TillHuvud = HplMedelStoraTat %>% 
   mutate(huvudort = ifelse(KommunNamn == "Håbo", "Bålsta",
                            ifelse(KommunNamn == "Älvkarleby", "Skutskär", KommunNamn))) %>%
@@ -94,21 +117,22 @@ df = TillHuvud %>%
   dplyr::select(StartHplID, StopHplID, Typ)
 
 
-### test att append fungerade
+## test att append fungerade
 # df %>% filter(StartHplID == "700600")
 # df %>% filter(StartHplID == "700600") %>% group_by(Typ) %>% tally()
 # df %>% filter(StopHplID == "700600")
 # df %>% filter(StopHplID == "700600") %>% group_by(Typ) %>% tally()
 
 
-#### API skript ####
+#---------------------------------------------------------------------------------------------------
+# Skapa API input fil
+#---------------------------------------------------------------------------------------------------
 time = c("19:00:00", "20:00:00", "21:00:00")
 timekort = c("19", "20", "21")
 
 # resmöjligheter behövs på vardagar och helger
 dateVardag = "2020-05-25"
 dateHelg = "2020-05-30"
-
 
 n = length(time)
 df1 = do.call("rbind", replicate(n, df, simplify = FALSE))
@@ -118,17 +142,17 @@ daterep = rep(dateVardag, each=length(timereplokal))
 
 df2 = data.frame(df1, daterep, timereplokal)
 
-# byt datum till helg och append data - vardag och helg resor i samma df
+## byt datum till helg och append data - vardag och helg resor i samma df
 df3 = df2 %>% 
   mutate(daterep = gsub(dateVardag, dateHelg, daterep)) %>%
   bind_rows(., df2)
 
-# konvertera tid till UTC
+## konvertera tid till UTC
 df3 = df3 %>% 
   mutate(DateTimeLokal = as.POSIXct(paste(daterep, timereplokal), tz="Europe/Berlin"),
          DateTimeUTC = format(DateTimeLokal, tz="UTC",usetz=TRUE))
 
-# skapa url för varje resa
+## skapa url för varje resa
 df3$url = paste0("https://api.ul.se/api/v4/journeys?",
                  "fromPointId=",df3$StartHplID,"&fromPointType=","0",
                  "&toPointId=", df3$StopHplID,"&toPointType=","0",
@@ -137,7 +161,12 @@ df3$url = paste0("https://api.ul.se/api/v4/journeys?",
                  "&trafficTypes=","1,2,3,4,5,6,7,8,9,10,11")
 
 
-# skicka frågor till API (1000 resor ~ 6min) 
+
+#---------------------------------------------------------------------------------------------------
+# Hämta data från API 
+#---------------------------------------------------------------------------------------------------
+
+## skicka frågor till API (1000 resor ~ 6min) 
 Start = list()
 Stop = list()
 StartHplID = list()
@@ -161,16 +190,22 @@ for(i in 1:nrow(df3)){
   })
 }
 
-##### slå ihopp allt i en df
+## slå ihopp allt i en df
 fin = do.call(rbind, Map(data.frame, 
                          Start=Start, Stop = Stop, StartHplID = StartHplID, StopHplID = StopHplID,
                          StartTid = StartTid, AnkomstTid = AnkomstTid, AntalBytePerResa = AntalBytePerResa))
 
 write.csv2(fin, "02_output_data/temp/aktiviteter.csv", row.names = F)
 
-# ta bort duplikater 
-# om det inte finns tillräckligt många turer per timma (API försöker alltid att hitta 6 turer per resa) hittar APIn turer på en annan tid 
-# om man sen söker efter turer kl 06 hittar man samma turer och antal turer får inte adderas 
+
+
+#---------------------------------------------------------------------------------------------------
+# Skapa resultat
+#---------------------------------------------------------------------------------------------------
+
+## ta bort duplikater 
+## om det inte finns tillräckligt många turer per timma (API försöker alltid att hitta 6 turer per resa) hittar APIn turer på en annan tid 
+## om man sen söker efter turer kl 06 hittar man samma turer och antal turer får inte adderas 
 fin1 = fin %>% distinct() 
 write.csv2(fin1, "02_output_data/temp/aktiviteter1.csv", row.names = F)
 
@@ -211,9 +246,8 @@ temp = AntalTurerPerBytePerDygnsTimma %>%
   dplyr::rename(StopTatort = TatortNamn) %>%
   left_join(., df, by =  c("StartHplID" = "StartHplID", "StopHplID" = "StopHplID"))
 
-#### Slutresultat #####
 
-# vilka resrelationer bör finnas
+## vilka resrelationer bör finnas
 Resrelation = medelstor.shp@data %>% 
   dplyr::select(MedelTatort = TATORT, HuvudTatort = KOMMUNNAMN) %>% 
   mutate(HuvudTatort = ifelse(HuvudTatort == "Håbo", "Bålsta",
@@ -221,7 +255,7 @@ Resrelation = medelstor.shp@data %>%
          concat = paste(MedelTatort, HuvudTatort, sep = "_"),
          concat_inv = paste(HuvudTatort, MedelTatort, sep = "_"))
   
-# vilka resrelationer finns
+## vilka resrelationer finns
 resultat = temp %>% 
   filter(AntalBytePerResa <= 1) %>% # max 1 byte
   group_by(Typ, StartTatort, StopTatort) %>% 
@@ -234,14 +268,14 @@ temp2 = resultat %>%
   ungroup() %>%
   dplyr::select(concat)
 
-# bör finnas men finns inte
+## bör finnas men finns inte
 saknas = Resrelation %>% filter(concat %notin% temp2$concat | 
                          concat_inv %notin% temp2$concat) %>% 
   mutate(Typ = "saknas",
          SummaAntalTurer = 0) %>%
   dplyr::select(Typ, StartTatort = MedelTatort, StopTatort = HuvudTatort, SummaAntalTurer)
 
-# slå ihop finns och finns inte
+## slå ihop finns och finns inte
 resultat2 = rbind(resultat, saknas)
 
 resultat2 = resultat2 %>% 
